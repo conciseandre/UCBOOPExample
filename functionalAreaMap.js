cdf43f58ff10e50dc74e1e2d9cb0494be53caa41
conciseandre <andrejdouglas@gmail.com>
2018-03-06 22:23:11 -0600

    Create functionalAreaMap.js

---
+++ functionalAreaMap.js
@@ -0,0 +1,28 @@
+var functionalAreaMap = {
+    "absence": "Absence",
+    "benefits" : "Benefits",
+    "compensation": "Compensation",
+    "functional_area__data_conversion": "Data Conversion",
+    "financials": "Financials",
+    "hcm": "HCM",
+    "functional_area__hcm__business_processes": "HCM",
+    "functional_area__hcm__general": "HCM",
+    "functional_area__hcm__reporting": "HCM",
+    "functional_area__hcm__security": "HCM",
+    "learning": "Learning",
+    "payroll": "Payroll",
+    "recruiting":"Recruiting",
+    "talent_performance":"Talent",
+    "time_tracking": "Time Tracking",
+    "ccb_integration": "Integration",
+    "integration__connector": "Integration",
+    "integration__delegated_authentication":"Integration",
+    "eib_integration":"Integration",
+    "report_based_integration":"Integration",
+    "studio_integration": "Integration",
+    "unknown_integration": "Integration",
+    "upcoming_wd_release_presentations": "Release Presentaton"
+ };
+ 
+ module.exports(functionalAreaMap);
+            
